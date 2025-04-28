35fb8cea440d15f9ccc3be71d12f46cbfdca93e3
psuresh314 <psureshkumar0131@gmail.com>
2025-04-28 22:33:10 +0000

    lab 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import productData from './data/full-products';
+import Header from './components/Header';
+import CardList from './components/CardList';
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There.</h1>
+      <Header />
+
     </div>
   );
 }
-
 export default App;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,49 @@
 const CardList = () => {
   return ();
+CardList.jsx
+
+
+import Card from './Card'
+import Button from './Button';
+import React, { useState, useEffect } from "react";
+
+const CardList = ({data}) => {
+  const limit = 10;
+  const defaultDataset = data.slice(0, limit);
+  const [offset, setOffset] = useState(0);
+  const [products, setProducts] = useState(defaultDataset);
+
+  const handlePrevious = () => {
+    // set the offset to the previous 10 products
+    setOffset(offset - 10);
+  }
+
+  const handleNext = () => {
+    // set the offset to the next 10 products
+    setOffset(offset + 10);
+  }
+
+  useEffect(() => {
+    // set the products state variable to the next 10 products
+    setProducts(data.slice(offset, offset + limit));
+  }, [offset, limit, data]);
+
+  return (
+    <div className="cf pa2">
+      <div className="mt2 mb2">
+      {data.map((product) => (
+        <Card key={product.id} {...product} />
+      ))}
+      </div>
+
+      <div className="flex items-center justify-center pa4">   
+        <Button text="Previous" handleClick={handlePrevious} />
+        <Button text="Next" handleClick={handleNext} />
+      </div>
+    </div>
+  )
 }
 
+export default CardList; 
 export default CardList;
+
